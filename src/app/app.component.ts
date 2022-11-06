d19daddb87689d42c6ca514da38262765e139ffa
dianaklr <diana.klr148@gmail.com>
2022-11-07 00:01:55 +0200

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:`
+  <div>Angular</div>
+  <nav>
+    <a routerLink="">Home</a> <br>
+    <a routerLink="/about">About</a>
+    <br>
+    <a routerLink="/news">News</a> <br>
+    <a routerLink="/price">Price</a>
+
+
+  </nav>
+<router-outlet></router-outlet>
+  `
 })
 export class AppComponent {
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {Routes, RouterModule} from "@angular/router";
+
 import { AppComponent } from './app.component';
 import { DateComponent } from './date/date.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NewsComponent } from './news/news.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { PriceComponent } from './price/price.component';
+
+
+// @ts-ignore
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'price', component: PriceComponent},
+  {path: '**', component: NotFoundComponent},
+]
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    DateComponent
+    DateComponent,
+    HomeComponent,
+    AboutComponent,
+    NewsComponent,
+    NotFoundComponent,
+    PriceComponent
   ],
+
   imports: [
-    BrowserModule
+    BrowserModule,
+    // @ts-ignore
+    RouterModule.forRoot(appRoutes)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
